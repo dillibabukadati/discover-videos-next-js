8395e48d2544e191de1843b5ae2d9d1eef513a21
dilli babu <dillibabukadati777@gmail.com>
2023-05-24 16:54:38 +0530

    fixed router issue

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import { useEffect, useState } from "react";
 import { magic } from "@/lib/magic";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   const [user, setUser] = useState();
-
+  const router = useRouter();
   useEffect(() => {
     const getUserDetails = async () => {
       const isLoggedIn = await magic.user.isLoggedIn();
